e3a8f3fca036618e53fa1369a8a62cbcd5d2e722
Taichi Maeda <taichi.maeda.up@gmail.com>
2025-01-14 17:04:41 +0000

    Update intros.v

--- theories/intros.v
+++ theories/intros.v
@@ -7,7 +7,7 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 Section intro_opsem.
-  
+
 Context `{ho: host}.
 
 (* Reduction of an instruction sequence.
@@ -17,7 +17,29 @@ Lemma opsem_reduce_seq1: forall hs1 s1 f1 es1 hs2 s2 f2 es2 es0,
       reduce hs1 s1 f1 es1 hs2 s2 f2 es2 ->
       reduce hs1 s1 f1 (es1 ++ es0) hs2 s2 f2 (es2 ++ es0).
 Proof.
-Admitted.
+   intros.
+   eapply r_label. exact H.
+   instantiate (1 := LH_base [::] es0).
+   all: simpl; reflexivity.
+Qed.
+
+Lemma e_to_v_inv : forall v,
+   is_const v -> v_to_e (e_to_v v) = v.
+Proof.
+   intros. destruct v. destruct b.
+   all: unfold is_const, e_to_v_opt in H; try discriminate H; reflexivity.
+Qed.
+
+Lemma e_to_v_list_inv : forall vs, 
+   const_list vs -> v_to_e_list (e_to_v_list vs) = vs.
+Proof.
+   intros. induction vs.
+   - reflexivity.
+   - simpl in H. unfold is_true in H. rewrite -> andb_true_iff in H. destruct H.
+     simpl. f_equal.
+     + apply e_to_v_inv. exact H.
+     + apply IHvs. exact H0.
+Qed.
   
 (* The same applies for attaching a list of values on the left. *)
 Lemma opsem_reduce_seq2: forall hs1 s1 f1 es1 hs2 s2 f2 es2 vs,
@@ -25,9 +47,41 @@ Lemma opsem_reduce_seq2: forall hs1 s1 f1 es1 hs2 s2 f2 es2 vs,
     reduce hs1 s1 f1 es1 hs2 s2 f2 es2 ->
     reduce hs1 s1 f1 (vs ++ es1) hs2 s2 f2 (vs ++ es2).
 Proof.
-Admitted.
+   intros.
+   eapply r_label. exact H0.
+   instantiate (1 := LH_base (e_to_v_list vs) [::]).
+   all: simpl; rewrite -> List.app_nil_r; f_equal; apply e_to_v_list_inv; exact H.
+Qed.
 
 Variable hs: host_state.
+
+Lemma vref_to_e_neq_nop : forall vref,
+   vref_to_e vref <> AI_basic BI_nop.
+Proof.
+   intros v H. destruct v;
+   simpl in H; discriminate H.
+Qed.
+
+Lemma v_to_e_neq_nop : forall v,
+   v_to_e v <> AI_basic BI_nop.
+Proof.
+   intros v H. destruct v;
+   solve [
+      simpl in H; discriminate H |
+      simpl in H; destruct v; simpl in H; discriminate H ].
+Qed.
+
+Lemma opsem_reduce_simple_nop : forall es1 es2,
+   reduce_simple (AI_basic BI_nop :: es1) (AI_basic BI_nop :: es2) -> False.
+Proof.
+   intros es1 es2 H.
+   inversion H; subst;
+   match goal with
+   | H : _ |- _ => solve [
+      apply vref_to_e_neq_nop in H; destruct H |
+      apply v_to_e_neq_nop in H; destruct H]
+   end.
+Qed.
 
 (* Is the above true without the const_list assumption?
    Prove or disprove it by establishing a witness to the following: *)
@@ -35,11 +89,55 @@ Lemma opsem_reduce_seq2':
     {forall s1 f1 es1 s2 f2 es2 es0,
     reduce hs s1 f1 es1 hs s2 f2 es2 ->
     reduce hs s1 f1 (es0 ++ es1) hs s2 f2 (es0 ++ es2)} +
-    {exists s1 f1 es1 s2 f2 es2 es0,
-    reduce hs s1 f1 es1 hs s2 f2 es2 ->
-    (reduce hs s1 f1 (es0 ++ es1) hs s2 f2 (es0 ++ es2) -> False)}.
+    {forall es1 es2, exists s1 f1 s2 f2 es0,
+    reduce hs s1 f1 es1 hs s2 f2 es2 /\
+    reduce hs s1 f1 (es0 ++ es1) hs s2 f2 (es0 ++ es2) -> False}.
 Proof.
+   apply right.
+   intros es1 es2.
+   set empty_record := Build_store_record [::] [::] [::] [::] [::] [::].
+   exists empty_record. exists empty_frame.
+   exists empty_record. exists empty_frame.
+   exists [::AI_basic BI_nop].
+   simpl. intros [H1 H2].
+   remember (AI_basic BI_nop :: es1) as esl.
+   remember (AI_basic BI_nop :: es2) as esr.
+   induction H2; 
+   (* TODO: Name variables *)
+   try solve [discriminate Heqesr |
+              injection Heqesl as Hcontra; discriminate Hcontra |
+              injection Heqesr as Hcontra; discriminate Hcontra ];
+   subst.
+   - eapply opsem_reduce_simple_nop. exact H.
+   - unfold result_to_stack in Heqesr. destruct r eqn:Er.
+      + destruct l eqn:El.
+         * simpl in Heqesr. discriminate Heqesr.
+         * simpl in Heqesr. injection Heqesr as Heqesr.
+           apply v_to_e_neq_nop in Heqesr. destruct Heqesr.
+   - injection Heqesr as Heqesr. discriminate Heqesr.
+   - apply IHreduce; clear IHreduce.
+      + exact H1.
+      + admit.
+      + admit.
 Admitted.
+(* 
+   apply right.
+   set empty_record := Build_store_record [::] [::] [::] [::] [::] [::].
+   exists empty_record. exists empty_frame.
+   exists [::].
+   exists empty_record. exists empty_frame.
+   exists [::]. exists [::AI_basic BI_nop].
+   simpl. intros H1 H2.
+   
+   inversion H2; subst.
+   - inversion H.
+   - rewrite -> H in H0. injection H0 as H0. discriminate H0.
+   - rewrite -> H in H0. injection H0 as H0. discriminate H0.
+   - rewrite -> H in H0. injection H0 as H0. discriminate H0.
+   (* - simpl in H3. unfold lookup_N in H3. Check List.nth_error_None. *)
+   - admit.
+   - rewrite -> H in H0. injection H0 as H0. discriminate H0.
+   - admit. *)
 
 End intro_opsem.
 
